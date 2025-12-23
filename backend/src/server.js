bae725b2c4a38dc76d4efd0e3043e19e2cff31fc
yusufriswan <yusufriswan01@gamail.com>
2025-12-23 12:59:16 +0530

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"; 
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -13,11 +14,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
-
+const __dirname = path.resolve()
+if(process.env.NODE_ENV !== "production") {
 app.use(cors({
     origin: "http://localhost:5173",
 }))
+}
 // middleware to parse JSON
 app.use(express.json());
 app.use(rateLimiter);
@@ -29,6 +31,14 @@ app.use(rateLimiter);
 //})
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))) 
+
+ app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+}
 
 connectDB().then(()=>{
     app.listen(PORT, () => {
