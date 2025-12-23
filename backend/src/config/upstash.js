33b9b7561d1653148561b040644f6ea96ff62057
yusufriswan <yusufriswan01@gamail.com>
2025-12-23 13:28:29 +0530

    third commit

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config();
 // Initialize Redis client
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, "20 s"),
+    limiter: Ratelimit.slidingWindow(100, "60 s"),
 });
 
 export default ratelimit;
