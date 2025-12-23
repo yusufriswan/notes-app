50771cc82c89b4d9c0953321e0636f4f2501fab6
yusufriswan <yusufriswan01@gamail.com>
2025-12-23 20:37:42 +0530

    vercel url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://notes-app-n.vercel.app/" : "/api";
 const api = axios.create({
     baseURL: BASE_URL,
 })
