4c5292bca910253bfde13a474ef242108787e073
yusufriswan <yusufriswan01@gamail.com>
2025-12-24 11:08:26 +0530

    env update

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve()
 if (process.env.NODE_ENV !== "production") {
   app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.APPLICATION_URL,
   }))
 }
 // middleware to parse JSON
