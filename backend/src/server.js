0d3dce1c12204de5122dca437045ad13379d79a3
yusufriswan <yusufriswan01@gamail.com>
2025-12-24 13:23:58 +0530

    upfsyr

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,51 +1,47 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path"
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
-
 dotenv.config();
-
-
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve()
+const __dirname = path.resolve();
+
+// middleware
 if (process.env.NODE_ENV !== "production") {
-  app.use(cors({
-    origin: process.env.APPLICATION_URL,
-  }))
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
-// middleware to parse JSON
-app.use(express.json());
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-
-//app.use((req,res,next) => {
-//console.log("We just got a new req")
-//next();
-//})
-
-app.get("/", (req, res) => {
-  res.json({ message: "Server is ready" });
-});
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV !== "production") {
-  connectDB().then(() => {
-    app.listen(PORT, () => {
-      console.log("Server is running on port:", PORT);
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-export default app;
-
-
-
-
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // in production, there's no localhost so we have to make this dynamic
-//const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-    baseURL: "http://localhost:5001",
+    baseURL: BASE_URL,
 })
 
 export default api;
