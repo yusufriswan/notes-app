d40cdc437fc3f86f4d9c90728dcdc3580fd6e19e
yusufriswan <119690890+yusufriswan@users.noreply.github.com>
2026-01-13 09:32:08 +0530

    Remove trailing slash from baseURL in axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // in production, there's no localhost so we have to make this dynamic
 //const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-    baseURL: 'https://notes-app-f969.vercel.app/',
+    baseURL: 'https://notes-app-f969.vercel.app',
 })
 
 export default api;
